5c9c5ae71f89b781ae67527bf8be3935cea740fb
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-08-20 03:21:39 +0530

    chore: Update Home component to fetch recipes from server and display them in cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle
-} from '@/components/ui/card'
+  CardTitle,
+} from "@/components/ui/card";
 
-
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface Recipe {
   title: string;
@@ -19,35 +21,42 @@ interface Recipe {
 }
 
 async function getRecipes(): Promise<Recipe[]> {
-  const result = await fetch('http://localhost:4000/recipes')
+  const result = await fetch("http://localhost:4000/recipes");
 
   return result.json();
 }
 
 export default async function Home() {
   const recipes = await getRecipes();
-  
+
   return (
-  <main>
-    <div className="grid grid-cols-3 gap-8">
-      {recipes.map(recipe=> (
-        <Card key={recipe.id} className="flex flex-col justify-between">
-          <CardHeader className="flex-row gap-4 items-center">
-            <div>
-              <CardTitle>{recipe.title}</CardTitle>
-              <CardDescription>{recipe.time} mins to cook 🍽️</CardDescription>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <p>{recipe.description}</p>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <button>View Recipe</button>
-            {recipe.vegan && <p>Vegan!</p> }
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
-  </main>    
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              {/* avatar */}
+                <Avatar>
+                  <AvatarImage src={`/img/${recipe.image}`} alt="recipe img" />
+                  <AvatarFallback>
+                    {recipe.title.slice(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook 🍽️</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="secondary">View Recipe</Button>
+              {recipe.vegan && <Badge>Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
   );
 }
